c243920677665e975a844931d753b9ccc34bb230
Alihan321231 <abdykerimovalihan490@gmail.com>
2024-01-21 12:30:21 +0600

    first commit

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 let flagdiv = document.querySelector('.flag');
-let getBtn = document.querySelector('.getBtn');
-
-getBtn.addEventListener('click', function () {
-    generateflag();
-  })
-  
-  getCnts();
-  
+let getbtn = document.querySelector('.getBtn');
 let countries = [];
 
-function getCnts() {
+getbtn.addEventListener('click', function() {
+let randomcountry = countries[Math.floor(Math.random() * countries.length)];
+let randomflag = randomcountry.flags.png;
+  Flag(randomflag);
+  let flags = JSON.parse(localStorage.getItem('flags')) || [];
+  flags.push(randomflag);
+  localStorage.setItem('flags', JSON.stringify(flags));
+});
+
+getcountries();
+
+function getcountries() {
   fetch('https://restcountries.com/v3.1/all')
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
       countries = data;
       displayFlags();
-    })
+    });
 }
 
 function displayFlags() {
-  let flags = localStorage.getItem('flags');
-  if (flags) {
-    flags = JSON.parse(flags);
-    flags.forEach((flag) => {
-      Flag(flag)
-    })
-  } else {
-    generateflag();
-  };
+  let flags = JSON.parse(localStorage.getItem('flags')) || [];
+  flags.forEach(Flag);
 }
 
 function Flag(flag) {
@@ -35,18 +32,3 @@ function Flag(flag) {
   img.src = flag;
   flagdiv.appendChild(img);
 }
-
-function generateflag() {
-  let randomind = Math.floor(Math.random() * countries.length);
-  let randomcntr = countries[randomind];
-  let randomflag = randomcntr.flags.png;
-  Flag(randomflag);
-  let flags = localStorage.getItem('flags');
-  if (flags) {
-    flags = JSON.parse(flags);
-    flags.push(randomflag);
-  } else {
-    flags = (randomflag);
-  
-  localStorage.setItem('flags', JSON.stringify(flags));
-}}
